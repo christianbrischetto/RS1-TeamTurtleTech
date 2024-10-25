e63d9ab86c19b1d05fd96870650c3a53fd21a5cc
Ryan-Stapleton <159881447+Ryan-Stapleton@users.noreply.github.com>
2024-10-25 16:20:08 +1100

    Launch file now works with slam toolbox, gazebo world, rviz/nav2, frontier exploration & person detection

--- turtle_tech_code/sprint_04/launch/turtle_tech_launch.py
+++ turtle_tech_code/sprint_04/launch/turtle_tech_launch.py
@@ -10,11 +10,34 @@ def generate_launch_description():
         # Set the TURTLEBOT3_MODEL environment variable
         SetEnvironmentVariable(name='TURTLEBOT3_MODEL', value='waffle_pi'),
 
+        # Include the slam_toolbox launch file
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(get_package_share_directory('slam_toolbox'), 'launch', 'online_async_launch.py')
+            )
+        ),
+        
         # Include the external launch file from the turtlebot3_gazebo package
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
                 os.path.join(get_package_share_directory('turtlebot3_gazebo'), 'launch', 'turtle_tech.launch.py')
             )
+        ),
+
+        # Include the nav2_bringup RViz launch file
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(get_package_share_directory('nav2_bringup'), 'launch', 'rviz_launch.py')
+            ),
+            launch_arguments={'use_sim_time': 'True'}.items()
+        ),
+
+        # Start the autonomous_exploration control node
+        Node(
+            package='autonomous_exploration',
+            executable='control',
+            name='autonomous_exploration_control',
+            output='screen'
         ),
 
         # Start the rgbDetect node
